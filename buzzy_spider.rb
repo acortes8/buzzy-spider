1a6067ee93825e2ca5d5c35c07d846ac35e6a3f0
acortes8 <46253800+acortes8@users.noreply.github.com>
2024-03-11 20:13:26 -0500

    Add url iterating through all valid recipe urls

--- buzzy_spider.rb
+++ buzzy_spider.rb
@@ -1,18 +1,14 @@
-require "httparty"
+require "open-uri"
 require "nokogiri"
+require "pry"
 
-response = HTTParty.get("https://www.liquor.com/claret-cup-8580464")
-html = response.body
     
 #puts html
 
-doc = Nokogiri::HTML(html)
 
-CocktailRecipe = Struct.new(:c_name, :c_image, :c_spirit_type, :c_description, :c_instruction, :i_name, :i_description, :ci_quantity, :ci_unit, :ci_description)
+#CocktailRecipe = Struct.new(:c_name, :c_image, :c_spirit_type, :c_description, :c_instruction, :i_name, :i_description, :ci_quantity, :ci_unit, :ci_description)
 
-html_article = doc.css("article")
-
-cocktail_recipes = []
+#cocktail_recipes = []
 
 =begin
 html_article.each do |html_article|
@@ -33,26 +29,100 @@ html_article.each do |html_article|
 end
 =end
 
-puts html_article.css("#heading_1-0 > h1").text
+url = "https://www.liquor.com/recipes/bloody-bulldog/"
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
 
-puts html_article.css("#article-header-tag-nav_1-0 > a.breadcrumbs__item.breadcrumbs__item--3").text
+c_name = doc.css("#heading_1-0 > h1")
+c_spirit_type = doc.css("#article-header-tag-nav_1-0 > a.breadcrumbs__item.breadcrumbs__item--3")
+c_description = doc.css("#article__header--project_1-0")
+ingredients_section = doc.css("#section--ingredients_1-0 > div")
+instructions_section = doc.css("#mntl-sc-block_3-0")
 
-selected_element = html_article.css("#article__header--project_1-0")
-paragraphs = selected_element.css('p').map(&:text)
-filtered_paragraphs = paragraphs.reject { |paragraph| paragraph.include?('<em>') }
-filtered_paragraphs.each do |paragraph|
-    puts paragraph
+if c_name.any? && c_spirit_type.any? && c_description.any? && ingredients_section.any? && instructions_section.any?
+    puts c_name.text
+    #puts c_spirit_type.text
+
+    paragraphs = c_description.css('p').map(&:text)
+    filtered_paragraphs = paragraphs.reject do |paragraph|
+        paragraph.include?("Reprinted with permission") ||
+        paragraph.include?(" / ")
+    end
+    filtered_paragraphs.each { |paragraph| puts paragraph }
+
+    #paragraphs = ingredients_section.css('p').map(&:text)
+    #paragraphs.each { |paragraph| puts paragraph }
+
+    #paragraphs = instructions_section.css('p').map(&:text)
+    #paragraphs.each { |paragraph| puts paragraph }
+else
+    puts "One or more elements are missing on the page."
 end
-puts html_article.css("#article__header--project_1-0")
 
-selected_element = html_article.css("#section--ingredients_1-0 > div")
-paragraphs = selected_element.css('p').map(&:text)
-paragraphs.each do |paragraph|
-    puts paragraph
+recipes = [
+  "Peels of 5 whole lemons",
+  "Peels of 1 orange",
+  "Peels of 2 whole grapefruits",
+  "1 whole pineapple, diced with skins",
+  "1 pound granulated sugar",
+  "1/8 teaspoon nutmeg, freshly grated",
+  "1/4 teaspoon coriander seeds",
+  "1 pod star anise",
+  "4 pods green cardamom, roasted",
+  "6 allspice berries, whole",
+  "1/8 teaspoon cloves, whole",
+  "16 ounces Lifted Spirits Jalapeño and Herb Vodka",
+  "16 ounces Cleveland Wheat Penny 1958 Bourbon",
+  "8 ounces Heirloom Pineapple Amaro",
+  "8 ounces Geijer California Fernet",
+  "6 ounces lemon juice, freshly squeezed",
+  "4 ounces pastis",
+  "1 quart oversteeped passion fruit green tea (instructions below)",
+  "1 1/4 quarts whole milk",
+  "Garnish: sweet pepper (optional)",
+  "Garnish: carnation flower (optional)"
+]
+
+recipe_info = []
+
+recipes.each do |recipe|
+  match = recipe.match(/^(?<quantity>[\d\s\/]+)?\s*(?<unit>\w+)?\s*(?<name>.+)$/)
+
+  if match
+    quantity = match[:quantity]
+    unit = match[:unit]
+    name = match[:name]
+
+    # If quantity is present and includes a '/', handle fractions
+    if quantity && quantity.include?('/')
+      numerator, denominator = quantity.split('/').map(&:to_i)
+      quantity = numerator.to_f / denominator
+    else
+      quantity = quantity.to_i if quantity
+    end
+
+    recipe_info << {
+      name: name,
+      quantity: quantity,
+      unit: unit,
+      description: recipe
+    }
+  else
+    # If the regular expression doesn't match, consider it as just a name/description
+    recipe_info << {
+      name: recipe,
+      quantity: nil,
+      unit: nil,
+      description: recipe
+    }
+  end
 end
 
-selected_element = html_article.css("#mntl-sc-block_3-0")
-paragraphs = selected_element.css('p').map(&:text)
-paragraphs.each do |paragraph|
-    puts paragraph
+# Print the parsed information
+recipe_info.each do |info|
+  puts "Name: #{info[:name]}"
+  puts "Quantity: #{info[:quantity]}"
+  puts "Unit: #{info[:unit]}"
+  puts "Description: #{info[:description]}"
+  puts "\n"
 end
--- sitemapscraper.rb
+++ sitemapscraper.rb
@@ -3,34 +3,61 @@ require "nokogiri"
 require "pry"
 
 class Scraper
-    def scrape_category_urls
-        sitemap_url = "https://www.liquor.com/sitemap_1.xml"
-        xml = URI.open(sitemap_url)
+    def scrape_potential_urls
+        sitemap = "https://www.liquor.com/sitemap_1.xml"
+        xml = URI.open(sitemap)
         doc = Nokogiri::XML(xml)
 
-        count = 0
-
-        recipe_urls = doc.css('url loc').map(&:text)
-        excluded_urls = File.readlines('excluded_urls.txt').map(&:chomp)
+        potential_urls = doc.css('url loc').map(&:text) #4284 urls
         
-        recipe_urls.reject! do |url|
-            url.include?("/articles/") ||
-            url.include?("/brands/") ||
-            url.include?("/slideshows/") ||
-            url.include?("best") ||
-
-            excluded_urls.any? { |excluded_url| url.include?(excluded_url) }
+        potential_urls.reject! do |url|
+            url.include?("/articles/") || #removes 765
+            url.include?("/brands/") || #removes 347
+            url.include?("/slideshows/") || #removes 196
+            url.include?("best") #removes 360
         end
 
-        recipe_urls.each do |url|
-            puts url
-            count += 1
+        scrape_potential_recipes(potential_urls)
+    end
+
+    def scrape_potential_recipes(potential_urls)
+        potential_urls.each do |potential_url|
+            url = potential_url
+            html = URI.open(url)
+            doc = Nokogiri::HTML(html)
+
+            c_name = doc.css("#heading_1-0 > h1")
+            c_spirit_type = doc.css("#article-header-tag-nav_1-0 > a.breadcrumbs__item.breadcrumbs__item--3")
+            c_description = doc.css("#article__header--project_1-0")
+            ingredients_section = doc.css("#section--ingredients_1-0 > div")
+            instructions_section = doc.css("#mntl-sc-block_3-0")
+
+            if c_name.any? && c_spirit_type.any? && c_description.any? && ingredients_section.any? && instructions_section.any?
+                puts "-------------------------------------------------------"
+                puts c_name.text
+                #puts c_spirit_type.text
+            
+                #paragraphs = c_description.css('p').map(&:text)
+                #filtered_paragraphs = paragraphs.reject do |paragraph|
+                    #paragraph.include?("Reprinted with permission") ||
+                    #paragraph.include?(" / ")
+                #end
+                #filtered_paragraphs.each { |paragraph| puts paragraph }
+
+                paragraphs = ingredients_section.css('p').map(&:text)
+                paragraphs.each { |paragraph| puts paragraph }
+                puts "-------------------------------------------------------"
+            
+                #paragraphs = instructions_section.css('p').map(&:text)
+                #paragraphs.each { |paragraph| puts paragraph }
+            else
+                puts "One or more elements are missing on the page."
+            end
+
+            puts 
         end
-
-        puts count
-
     end
 end
 
 scrape = Scraper.new
-scrape.scrape_category_urls
+scrape.scrape_potential_urls
